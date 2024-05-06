997a9569b7a38c50a4a68026fe459b8746682b4f
Jenny V Medina <jenny.medina@sagebase.org>
2024-05-06 10:44:57 -0400

    Update score.py

--- score.py
+++ score.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 
 import glob
@@ -14,14 +15,13 @@ SCORED = "SCORED"
 
 def score_submission(predictions_path: str, status: str) -> typing.Tuple[str, dict]:
     """Determine the score of a submission. This is a placeholder function.
-
     Args:
         predictions_path (str): path to the predictions file
         status (str): current submission status
-
     Returns:
         result (dict): dictionary containing score, status and errors
     """
+
     if status == INVALID:
         score_status = INVALID
         score1, score2, score3 = None, None, None
@@ -75,7 +75,6 @@ def score_submission(predictions_path: str, status: str) -> typing.Tuple[str, di
 
 def update_json(results_path: str, result: dict) -> None:
     """Update the results.json file with the current score and status
-
     Args:
         results_path (str): path to the results.json file
         result (dict): dictionary containing score, status and errors
@@ -90,8 +89,11 @@ def update_json(results_path: str, result: dict) -> None:
 
 if __name__ == "__main__":
     predictions_path = sys.argv[1]
-    results_path = sys.argv[2]
-    status = sys.argv[3]
+    goldstandard = sys.argv[2]
+    result_path = sys.argv[3]
+
+    status = result_path["validation_status"]
+
     score_status, result = score_submission(predictions_path, status)
-    update_json(results_path, result)
+    update_json(result_path, result)
     print(score_status)
